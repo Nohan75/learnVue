16893aa2d7f7abaa8322d9cbe8ff636a4adb96b7
Nohan75 <nohanmarielouise@gmail.com>
2020-11-10 10:50:20 +0100

    Componant Shop and search part

--- vue.js
+++ vue.js
@@ -1,19 +1,24 @@
 
 const products = [
-    {id: 1, name: 'Hamburger bacon', price: 10, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 2, name: 'Cheeseburger', price: 11, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 3, name: 'Cheeseburger bacon', price: 9, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 4, name: 'Pizza 4 fromages', price: 12, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 5, name: 'Pizza reine', price: 14, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 6, name: 'Pizza peperoni', price: 15, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 7, name: 'Tacos M', price: 12, img: 'https://source.unsplash.com/random/200x200'},
-    {id: 8, name: 'Tacos XL', price: 15, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 1, categ: 'burger', name: 'Hamburger bacon', price: 10, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 2, categ: 'burger', name: 'Cheeseburger', price: 11, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 3, categ: 'burger', name: 'Cheeseburger bacon', price: 9, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 4, categ: 'pizza', name: 'Pizza 4 fromages', price: 12, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 5, categ: 'pizza', name: 'Pizza reine', price: 14, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 6, categ: 'pizza', name: 'Pizza peperoni', price: 15, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 7, categ: 'tacos', name: 'Tacos M', price: 12, img: 'https://source.unsplash.com/random/200x200'},
+    {id: 8, categ: 'tacos', name: 'Tacos XL', price: 15, img: 'https://source.unsplash.com/random/200x200'},
 ]
 
 
 const Homepage = {
-    template: '#home',
-    name: 'Home',
+    template: '<h1>Home</h1>',
+    name: 'Home'
+}
+
+const Shop = {
+    template: '#shop',
+    name: 'Shop',
     data: () => {
         return {
             products,
@@ -39,6 +44,7 @@ const Contact = {
 
 const routes = [
     { path: '/', component: Homepage, name: 'Home' },
+    { path: '/shop', component: Shop, name: 'Shop' },
     { path: '/settings', component: Settings, name: 'Settings' },
     { path: '/contact', component: Contact, name: 'Contact' }
 ]
